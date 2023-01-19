d37584b6364a144efde11d0122bfb0e71f38cc75
LeRenzo Malcom <lerenzo@miro.com>
2023-01-19 07:21:08 -0600

    Update push-docker

--- ci/push-docker.sh
+++ ci/push-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker login ghcr.io --username "$docker_username" --password "$docker_password"
+echo "$docker_password" | docker login ghcr.io --username "$docker_username" --password-stdin
 docker push "ghcr.io/$docker_username/micronaut-app:1.0-${GIT_COMMIT::8}" 
 docker push "ghcr.io/$docker_username/micronaut-app:latest" &
 wait
